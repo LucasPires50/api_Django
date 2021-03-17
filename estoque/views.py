7fa6dda8b2f3b6d1f6a8fd65004f13e41b71f1a0
Lucas Pires <luc_50@outlook.com>
2021-03-16 22:20:34 -0300

    ajustes

--- estoque/views.py
+++ estoque/views.py
@@ -3,7 +3,7 @@ from estoque.models import EstoqueInterno
 from estoque.serializer import EstoqueSerializer
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.decorators import action
+
 
 
 
