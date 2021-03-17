7143005f4df5b6f09c8a70212cf4124c4200893e
Lucas Pires <luc_50@outlook.com>
2021-03-16 21:33:27 -0300

    ajustes

--- estoque/views.py
+++ estoque/views.py
@@ -1,12 +1,18 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from estoque.models import EstoqueInterno
 from estoque.serializer import EstoqueSerializer
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+
+
 
 class EstoqueViewSet(viewsets.ModelViewSet):
     queryset = EstoqueInterno.objects.all()
     serializer_class = EstoqueSerializer
     authentication_classes = [BasicAuthentication]
     permission_classes = [IsAuthenticated]
+
+
+
 
